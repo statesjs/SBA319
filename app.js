f8166985540692c94f6a093be2bf37f86c9969ea
statesjs <statesjs@gmail.com>
2025-04-23 08:03:50 -0700

    adding user routes

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5050;
 //  Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//users ROUTER
+const userRoutes = require("./routes/userRoutes");
+//implementing routes
+app.use("/api/users", userRoutes);
 //ROOT get request
 app.get("/", (req, res) => {
   res.json("Welcome to the DevLink API!");
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,25 @@
 const express = require("express");
 const User = require("../models/user.model");
 const router = express.Router();
+
+//GET
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//POST
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
